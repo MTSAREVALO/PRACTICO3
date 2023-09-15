0ea0f1eb8fddbf3ba6fcc6a5d647cbd0d6e35e09
MTSAREVALO <mtsarevalo@gmail.com>
2023-09-14 21:06:11 -0300

    Updates

--- src/componentes/football.jsx
+++
@@ -1,9 +0,0 @@
-function football(){
-    const shot= () =>{
-        alert ("gratest shoot");
-    }
-    return(
-        <button onclick="{shot}">take the shoot</button>
-    );
-}
-export default football
